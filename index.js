fa8fbae9f25e7327fe3122a2fc97e4a150409496
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2023-05-22 18:51:48 +0200

    index.js modified

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
+
 
 const app = express();
 app.use(express.json());
@@ -11,14 +13,13 @@ app.post("/authenticate", async (req, res) => {
     const response = await axios.put(
       "https://api.chatengine.io/users/",
       { username: username, secret: username, first_name: username },
-      { headers: { "Private-Key": "c05c5b26-a8e9-43d1-bc5e-ce8872a8310d" } }
+      { headers: { "private-Key": "b1bb6b75-23f6-44ad-aea0-292f78ed3bc0" } }
     );
     return res.status(response.status).json(response.data);
   } catch (error) {
     return res.status(error.response.status).json(error.response.data);
   }
 
-  return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
